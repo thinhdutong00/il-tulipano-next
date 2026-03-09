1b3787125df7dd49f8c74e330908240c69564413
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-03-09 17:52:16 +0100

    fix

--- app/informazioni/page.tsx
+++ app/informazioni/page.tsx
@@ -10,7 +10,7 @@ export default function InformazioniPage() {
       <section className="relative h-[60vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0 bg-black/30 z-10" />
         <Image 
-          src="/scaffale-dei-vini-tulipano.webp" 
+          src="/tulipanocarpi-grappa.webp" 
           alt="Sfondo Informazioni"
           fill
           className="object-cover"
@@ -237,7 +237,7 @@ export default function InformazioniPage() {
           </div>
         </div>
       </section>
-      
+
     </main>
   );
 }
